51ffffaf578ae4f0f4f02b82fca329fc5840153f
Turner-Tobie <turner.tobie@yahoo.com>
2016-09-27 09:57:54 -0400

    JavaLibrary

---
+++ src/_02_Hello_World/Hello_World.java
@@ -0,0 +1,35 @@
+package _02_Hello_World;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		System.out.println("syso-ctrl+space=shortcut for line");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
