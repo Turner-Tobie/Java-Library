a6cb4d3d7ca809e18e17306b3e02fdded777d2f1
Turner-Tobie <turner.tobie@yahoo.com>
2016-09-30 12:00:41 -0400

    additional java stuff

---
+++ src/_09_Loops/_02_1_ForLoopPractice.java
@@ -0,0 +1,83 @@
+package _09_Loops;
+
+public class _02_1_ForLoopPractice {
+
+	public static void main(String[] args) {
+		
+
+		
+
+			   //for (int i = 0; i <= 10; i+=2) {
+			   //System.out.println(i);
+			   //}
+			   
+			   
+			   //for (int i = 1; i <=30; i+=3){
+			   //	System.out.println("Awesome");
+			   //}
+			   
+			   //for (int i = 1; i <=30; i++)
+			   //	if((i%3 == 0)){
+			   //	 System.out.println("Awesome");
+			   //}else{
+			   //	System.out.println(i);
+			   //}
+			   
+			   
+			   //for (int i = 0; i <=100; i+=7)
+			   //	if (i==28||i==49){
+			   //		continue;     //can be used to skip selected
+			   	 
+			   //	}else{
+			   //		System.out.println(i);
+			   //	}
+			   
+			   //for (int i = 0; i <=100; i++)
+			   //	if ((i%5==0) && (i%3==0)){
+			   //		System.out.println("FizzBuzz");
+			   //	}else if(i%5==0){
+			   //		System.out.println("Fizz");
+			   //	}else if(i%3 == 0){
+			   //		System.out.println("Buzz");
+			   //	}else{
+			   //		System.out.println(i);
+			   //	}
+			   
+			   
+			   //for(int i = 1; i <= 10; i++)
+			   //		if(i == 5){
+			   //			System.out.println("I'm alive!");
+			   //		}else{
+			   //			System.out.println("The number is " + i);
+			   //		}
+			    
+			  // for(int d = 1 ; d <= 24 ; d++){
+			  // 		System.out.println(d + " ");
+			  // 	for(int x = 1; x <= 60 ; x++)
+			  // 		System.out.print(x + " ");
+			  // 		for (int y = 1; y <= 60 ; y++)
+			  // 		 System.out.print(y ) ;
+			  // 		 System.out.print("\n");
+			   		  
+			  //}
+			  
+			  //for(double i = 0; i <= 100 ; i += 2.5){
+			  
+			  //	if(i%25 == 0){
+			  //		System.out.println("Hey Paul");
+			  //	}else{
+			  //	System.out.println(i);
+			  //	}
+			  //}
+			   
+			   
+			   
+			  }
+			}
+		
+		
+		
+		
+	}
+
+}
